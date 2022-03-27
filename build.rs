c1c093857263ede03da73459f3ff5dbe7b1481a7
carlosb1 <mcflurry0@gmail.com>
2022-03-27 20:14:24 +0200

    Added skeleton to create dlpack

--- build.rs
+++ build.rs
@@ -4,26 +4,12 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    // println!("cargo:rustc-link-lib=bz2");
-
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("dlpack.h")
-        // Finish the builder and generate the bindings.
+        .header("dlpack/include/dlpack/dlpack.h")
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("dlpack.rs"))
         .expect("Couldn't write bindings!");
